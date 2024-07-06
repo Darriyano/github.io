30be8e1c9e4bc56607981107bcd85748ed542023
Darriyano <74460805+Darriyano@users.noreply.github.com>
2024-07-06 23:24:44 +0300

    Delete front-task5 directory

--- front-task5/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function customGoto(url: string) {
-  const a = document.createElement("a")
-  a.href = url
-  a.style.display = "none"
-  document.body.appendChild(a)
-  a.click()
-  document.body.removeChild(a)
-}
--- front-task5/src/routes/comic/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import Comic from "$lib/components/comic.svelte"
-</script>
-
-<Comic />
